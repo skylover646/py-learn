7dc7a164c60add037107b3fb9abfa128c207fe6a
skylover646 <akpatel64000@gmail.com>
2024-10-13 12:08:29 +0530

    new additon file

---
+++ arthmatic.py
@@ -0,0 +1,13 @@
+#arthmetic operator are +,-,/,*,%
+#first addition 
+age1=25
+age2=10
+print(age1+age2)
+#substraction
+print (age1-age2)
+#abstraction
+print(age1*age2)
+#division
+print(age1/age2)
+#modulation
+print(age1%age2)
