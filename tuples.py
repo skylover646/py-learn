01b98dccab75a5243a6396b2e199d1fc307d83a8
skylover646 <akpatel64000@gmail.com>
2024-10-13 16:50:50 +0530

    add one more

---
+++ tuples.py
@@ -0,0 +1,6 @@
+#tuples is immutable.it is constant value
+tup=('bannna','orange','apple')
+#if we want update it it is shown error.like these tup[0]='grapes'
+tup1=(12,15)
+tup3=tup+tup1
+print(tup3) 
