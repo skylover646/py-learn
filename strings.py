b009f8f3f73a879558e010f09880e68f8e8743d3
skylover646 <akpatel64000@gmail.com>
2024-10-13 12:25:47 +0530

    second file add

---
+++ strings.py
@@ -0,0 +1,14 @@
+#strings learning 
+sent1="today is beutiful day"
+print (sent1)
+#two strings addittion 
+first_name="sky"
+last_name="ptl"
+print(first_name+last_name)
+#how to add space between two strings 
+print (first_name+"   "+last_name)
+#write 10times hi 
+print ("hi"*10)
+#character index zero
+sent="sky is playing cricket."
+print(sent[0:3])
