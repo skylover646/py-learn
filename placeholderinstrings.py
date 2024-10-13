078472a5e0c1cf55b3a2f8e742a8a86d239a7eaa
skylover646 <akpatel64000@gmail.com>
2024-10-13 14:02:13 +0530

    new file add

---
+++ placeholderinstrings.py
@@ -0,0 +1,18 @@
+name="sky"
+#print(name+" "+"is 18 years old")
+#use  %s as name value
+sentence="%s is 18 years old"
+print (sentence % name)
+#use %s %s for name value add 
+sent="%s %s is working on python"
+print (sent % ("n1","ptl"))
+# s=string and d=integer
+sent1="%s is %d years old"
+print(sent1%("sky",23))
+#formate strings
+name="sky"
+print(f"Hello,{name}")
+#sum
+x=20
+y=30
+print(f"the sum of x and y is {x+y} ")
