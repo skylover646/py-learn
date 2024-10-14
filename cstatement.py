941243c5322e4016714dd2fd243abc3bc77d287e
skylover646 <akpatel64000@gmail.com>
2024-10-14 23:06:26 +0530

    some files modify and add

---
+++ cstatement.py
@@ -0,0 +1,18 @@
+#if 2>1:
+    #print ("true")
+#else:
+    #print("false")
+#relation opeartor
+# < less than
+# > greater than 
+# <= less or equal
+# >= greater equal
+# != not equal
+# == equal too
+age=21
+if age<13 :
+    print("you are child")
+elif age>=13 and age<=18:
+    print("you are teenager")
+else:
+    print("you are adult")
--- tuples.py
+++ tuples.py
@@ -3,4 +3,4 @@ tup=('bannna','orange','apple')
 #if we want update it it is shown error.like these tup[0]='grapes'
 tup1=(12,15)
 tup3=tup+tup1
-print(tup3) 
+print(tup3 ) 
